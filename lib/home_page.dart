9f16a4c528392b40f865b4db1ffb9d7b32a113df
Chinmay <chinmaymp007@gmail.com>
2022-12-04 02:08:05 +0530

    Layout made for the home page, i.e. skeleton added without icons

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+const bottomCheight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -15,10 +19,75 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: Color(0xFF0A0E21),
       appBar: AppBar(
-        title: Text(widget.title),
+
+        title: Center(
+          child: Text(widget.title),
+        ),
+        backgroundColor: Color(0xFF0A0E21),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Reausable(
+                colour: activeCardColor,
+              )),
+              Expanded(child: Reausable(
+                colour: activeCardColor,
+              )),
+            ],
+          ),),
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Reausable(
+                colour: activeCardColor,
+              ))
+            ],
+          ),),
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Reausable(
+                colour: activeCardColor,
+              )),
+              Expanded(child: Reausable(
+                colour: activeCardColor,
+              )),
+            ],
+          ),),
+          Container(
+            color: Colors.red,
+            margin: EdgeInsets.only(top: 5.0),
+            width: double.infinity,
+            height: bottomCheight,
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+class Reausable extends StatelessWidget {
+  Reausable({required this.colour});
+  // final Widget cardChild ;
+  final Color colour ;
+  @override
+  Widget build(BuildContext context) {
+    return Container(margin: EdgeInsets.all(10),
+      // child: cardChild,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
+      ),
     );
   }
 }
